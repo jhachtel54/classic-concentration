f6744bd2e1d2983eff6deacc35a97a026bdf385c
jhachtel54 <james@poppy.works>
2024-11-26 22:26:42 -0700

    Starting to make grid

--- www/js/index.js
+++ www/js/index.js
@@ -22,8 +22,37 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    var playArea = document.getElementById('playArea');//.classList.add('ready');
+	for (var rowIndex = 0; rowIndex < 5; ++rowIndex)
+	{
+		var row = document.createElement("div");
+		row.classList.add("row");
+		row.classList.add("h-20");
+		playArea.appendChild(row);
+		for (var colIndex = 0; colIndex < 5; ++colIndex)
+		{
+			var col = document.createElement("div");
+			col.classList.add("col");
+			col.classList.add("puzzlePanel");
+			row.appendChild(col);
+			
+			var inner = document.createElement("div");
+			inner.classList.add("puzzlePanelInner");
+			inner.classList.add("text-center");
+			col.appendChild(inner);
+			
+			var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+			svg.setAttributeNS('http://www.w3.org/2000/svg','viewBox','0 0 100 100');
+			svg.setAttributeNS('http://www.w3.org/2000/svg','preserveAspectRatio','none');
+			svg.classList.add("puzzlePanelInnerSVG");
+			inner.appendChild(svg);
+			svg.innerHTML = "<path d=\"M0 0 l100 0 l0 100 l-100 0 z\" style=\"fill:black;\" />";
+			
+			// var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+			// svg.setAttributeNS('http://www.w3.org/2000/svg','viewBox','0 0 100 100');
+			// svg.classList.add("puzzlePanelInnerSVG");
+			// inner.appendChild(svg);
+			// svg.innerHTML = "<path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z\"/>";//"<path=\"M0 0 L463 0 L463 214 L0 214 Z\" style=\"fill:black\">";
+		}
+	}
 }

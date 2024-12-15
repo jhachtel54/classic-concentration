5497d03c556bd21ef8913aa68d933f693590de35
James Hachtel <james@poppy.works>
2024-12-14 18:26:51 -0700

    Updated copy script to have more sync-like behavior and removed old files

--- hooks/appBrowserCopyFromPlatformToWWW.sh
+++ hooks/appBrowserCopyFromPlatformToWWW.sh
@@ -2,7 +2,7 @@
 while true; do
     read -p "Do you wish to copy over the working copies to source? " yn
     case $yn in
-        [Yy]* ) cp -R platforms/browser/www/index.html platforms/browser/www/css platforms/browser/www/img platforms/browser/www/js www/; break;;
+        [Yy]* ) rm -rf www/*; cp -R platforms/browser/www/index.html platforms/browser/www/css platforms/browser/www/img platforms/browser/www/js www/; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
